f3c03ee359c237d0304d6d464aadce07db65900a
TamiresDellaRovere <tamires.rovere@hotmail.com>
2023-04-17 16:52:51 -0300

    Relacionando Cliente e Endereco

---
+++ database/cliente.js
@@ -0,0 +1,57 @@
+// Modelo para gerar tabela de clientes no MySQL;
+// Mapeamento: cada propriedade vira uma coluna da tabela;
+
+//DataTypes = servir para definir tipo da coluna;
+const {DataTypes} = require("sequelize"); // traz as opções de tipos de coluna existentes no MySQL; Ex: STRING(VARCHAR), INT, CHAR;
+const {connection} = require("./database"); // traz a conecctions do arquivo database.js
+
+
+const Cliente = connection.define("cliente", {
+    // a coluna vai ser nome: abre o {} para definir as caracteristicas da coluna;
+    nome: {
+        type: DataTypes.STRING(130), //define tipo varchar com limite de 130 caracteres;
+        allowNull: false, //aplica o NOT NULL na coluna, o campo se torna obrigatório.
+    }, // essas configurações corresponde: nome VARCHAR(130) NOT NULL no MySQL;
+    email: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        unique: true //Define que não pode ter outro valor igual, é unico;
+    }, // email VARCHAR UNIQUE NOT NULL
+    telefone: {
+        type: DataTypes.STRING, //quando deixa o STRING vazio sem definição de caractere, ele mantem o maximo que é 255;
+        allowNull: false,
+    },// telefone VARCHAR NOT NULL;
+});
+
+
+// Associação 1:1 (One-to-One);
+const Endereco = require("./endereco");
+
+//ligação entre as tabelas de Cliente e Endereco:
+// cliente tem um Endereço
+// Endereço ganha uma chave estrangeira (nome do model + id) -> aqui no caso é "cliente_id"
+// Chave estrangeira = clienteId
+Cliente.hasOne(Endereco); // cliente tem um Endereço
+Endereco.belongsTo(Cliente); // endereço pertence a um Client
+
+
+module.exports = Cliente;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cliente = {
+//     // coluna: "valor"
+//     nome: "Tamires Sanchez", //VARCHAR no MySQL
+//     email: "tamires@gmail.com", //VARCHAR no MySQL
+//     telefone: "(17) 9-9999-9999" //VARCHAR no MySQL
+// }
--- index.js
+++ index.js
@@ -17,15 +17,38 @@ app.use(express.json()); //Possibilitar transitar dados usando JSON;
 //Configuração do Banco de Dados;
 const {connection, authenticate, autenticacao} = require("./database/database");
 autenticacao(connection); // efetivar a conexão;
+const Cliente = require("./database/cliente"); // Após criar o o arquivo Cliente.Js, importar para o index.js, logo abaixo da função de configuração do Database;
+const Endereco = require("./database/endereco");
 
 
 
 
 //Definição de rotas;
+app.post("/clientes", async(req, res) => {
+    const {nome, email, telefone, endereco } = req.body; //Coletar os dados do req.body;
+
+    try{ //se dar certo adiciona na tabela;
+        const novoCliente = await Cliente.create(
+            {nome, email, telefone, endereco},
+            {include: [Endereco]} // permite inserir cliente e endereço num comando -> pega os dados adicionado no objeto endereco do postman e adiciona na tabela Endereco do MySQL;
+            );
+        res.status(201).json(novoCliente);
+
+    } catch (err) { // se der erro aciona o catch e informa o erro;
+        console.log(err);
+        res.status(500).json({message: "um erro aconteceu."});
+    }
+}); // tem que ter o try catch para não travar o código, ele para o código e retorna o erro caso não de certo;
+
 
 
 
 //Escuta de eventos (listen);
 app.listen(3000, () => {
+    // Force = apaga tudo e recria as tabelas;
+    connection.sync({force: true}) // Gerar as tabelas a partir do model;
     console.log("Servidor rodando em http://localhost:3000")
 });
+
+
+
